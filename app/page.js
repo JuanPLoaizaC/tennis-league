948c34c9456356a201432e2d67aa8cc0348769c9
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-11-18 16:13:27 -0500

    Making changes to the landing page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import TennisRacquet from '../public/TennisRacquet.png';
-import { Bars3Icon } from '@heroicons/react/24/outline';
+import TennisRacquet from '@public/TennisRacquet.png';
 import Link from "next/link";
 
 const navigation = [
   { name: 'Sign up', href: 'auth/signup' },
-  { name: 'Log in', href: 'auth/login' },
+  { name: 'Sign in', href: 'auth/signin' },
 ]
 
 export default function Example() {
@@ -15,15 +14,12 @@ export default function Example() {
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image
-                src={TennisRacquet}
-                alt=""
-                width={64}  // ajusta el ancho según tus necesidades
-                height={64} // ajusta la altura según tus necesidades
-              />
-            </a>
+            <Image
+              src={TennisRacquet}
+              alt=""
+              width={64}  // ajusta el ancho según tus necesidades
+              height={64} // ajusta la altura según tus necesidades
+            />
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
