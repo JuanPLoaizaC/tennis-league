a999c99d00903b6afc9c4a5d8dd40e52b9153d36
JuanPLoaizaC <juanpablo9730@gmail.com>
2023-11-20 20:27:22 -0500

    Fixing the query to return the role's user

--- app/api/auth/signin/route.js
+++ app/api/auth/signin/route.js
@@ -4,7 +4,20 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   try {
       let body = await await req.json();
-      let query = `SELECT id, name, email FROM "TennisLeague".users where email = '${body.email}' and password = '${body.password}'`;
+      let query = `SELECT
+                      u.id AS id,
+                      u.name AS name,
+                      u.email AS email,
+                      r.name AS role
+                  FROM
+                      "TennisLeague".users u
+                  JOIN
+                      "TennisLeague".users_roles ur ON u.id = ur.user_id
+                  JOIN
+                      "TennisLeague".roles r ON ur.role_id = r.id
+                  WHERE
+                      u.email = '${body.email}' AND
+                      u.password = '${body.password}'`;
       const result = await pool.query(query);
       if (result.rows.length > 0) {
         return NextResponse.json({ user: result.rows[0] }, { status: 201 });
